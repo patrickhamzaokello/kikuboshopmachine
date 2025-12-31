748987ef469751827871c6a1d181d1252834cdf4
pks <pklab2>
2025-12-31 13:48:54 +0300

    update on shure

--- django.sh
+++ django.sh
@@ -13,7 +13,7 @@ python manage.py migrate --noinput || {
 
 echo "🚀 Starting Gunicorn server"
 exec gunicorn kikuboposmachine.wsgi:application \
-  --bind 0.0.0.0:6200 \
+  --bind 0.0.0.0:6300 \
   --workers 3 \
   --access-logfile - \
   --error-logfile -
